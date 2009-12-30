2ec8085310474c3752cb35408b45ce99cf64af0a
Christian Stocker <me@chregu.tv>
2009-12-30 07:39:11 +0100

    remove bxe for organising with braid

--- webinc/bxe/div/updateBuildDate.sh
+++
@@ -1,1 +0,0 @@
-date=`date +%Y%m%d%H%M`; sed -e "s/var BXE_BUILD = .*/var BXE_BUILD = \"$date\"/" bxeLoader.js > bxeLoader.js.new ; mv bxeLoader.js.new bxeLoader.js
--- webinc/bxe/firefox_extension/config_build.sh
+++
@@ -1,8 +0,0 @@
-APP_NAME=bxehelper
-CHROME_PROVIDERS="content"
-CLEAN_UP=1
-ROOT_FILES="readme.txt"
-ROOT_DIRS=
-BEFORE_BUILD=
-AFTER_BUILD=
-
